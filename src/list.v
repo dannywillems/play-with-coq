10f75f7c2373a6761ee058743780188375e4d75b
Danny Willems <be.danny.willems@gmail.com>
2024-10-12 22:47:40 +0200

    Add count

--- src/list.v
+++ src/list.v
@@ -4,3 +4,12 @@ Check 1::2::3::nil.
 
 Compute
   map (fun x => x + 2) (1::2::3::nil).
+
+(* 6. Proving properties of programs on lists *)
+
+Fixpoint count (n : nat) (l : list nat) : nat :=
+  match l with
+  | nil => 0
+  | h::tl =>
+      let r := count n tl in if Nat.eqb n h then 1 + r else r
+  end.
