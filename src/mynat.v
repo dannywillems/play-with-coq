746196336ef922a8db6023db6ca9ac50e0ba7dee
Danny Willems <be.danny.willems@gmail.com>
2024-10-12 22:33:51 +0200

    Move require to top-level

--- src/mynat.v
+++ src/mynat.v
@@ -1,3 +1,19 @@
+(* See https://coq.inria.fr/doc/V8.4pl6/refman/Reference-Manual027.html
+  Otherwise, we get
+  > Error: Cannot find a declared ring structure over "nat".
+
+```
+The tactic must be loaded by Require Import Ring. The ring structures must be
+declared with the Add Ring command (see below). The ring of booleans is
+predefined; if one wants to use the tactic on nat one must first require the
+module ArithRing (exported by Arith); for Z, do Require Import ZArithRing or
+simply Require Import ZArith; for N, do Require Import NArithRing
+or Require Import NArith.
+```
+ *)
+Require Import Ring.
+Require Import ArithRing.
+
 Definition is_zero (n: nat) : bool :=
   match n with
   | 0 => true
@@ -34,22 +50,6 @@ Fixpoint evenb (m: nat) : bool :=
 
 (* Print evenb. *)
 
-Require Import Ring.
-(* See https://coq.inria.fr/doc/V8.4pl6/refman/Reference-Manual027.html
-  Otherwise, we get
-  > Error: Cannot find a declared ring structure over "nat".
-
-```
-The tactic must be loaded by Require Import Ring. The ring structures must be
-declared with the Add Ring command (see below). The ring of booleans is
-predefined; if one wants to use the tactic on nat one must first require the
-module ArithRing (exported by Arith); for Z, do Require Import ZArithRing or
-simply Require Import ZArith; for N, do Require Import NArithRing
-or Require Import NArith.
-```
-*)
-
-Require Import ArithRing.
 
 (* 2 * (n * (n + 1)) / 2 = n^2 + n *)
 (* Lemma sum_n_p: forall n: nat, 2 * sum_n n = n * n + n. *)
