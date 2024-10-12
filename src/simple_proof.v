28d370138d938d2329783a3fc50311d4e75fc159
Danny Willems <be.danny.willems@gmail.com>
2024-10-12 19:17:57 +0200

    Add some comments to remember the meaning of Qed.

--- src/simple_proof.v
+++ src/simple_proof.v
@@ -1,3 +1,8 @@
+(** When we write Qed., it means that the lemma is going to be saved as a
+    theorem, and saved in the environment as a true statement.
+    It will allow us to use it later.
+ *)
+
 Lemma example2: forall a b: Prop, a /\ b -> b /\ a.
 Proof.
   intros a b H.
